abeda7e254f6c8425f415127681b289c010698d9
thavamani18 <thavamani.tamtree@gmail.com>
2025-03-07 18:01:54 +0530

    multiplication

--- manipulation.py
+++ manipulation.py
@@ -5,7 +5,13 @@
 # print("The addition of two number is",c)
 
 #  SUBTRACTION
+# a=5
+# b=4
+# c=a-b
+# print("The subtraction of two number is",c)
+
+# MULTIPLICATION
 a=5
 b=4
-c=a-b
-print("The subtraction of two number is",c)
+c=a*b
+print("The multiplication of two number is",c)
