7f0d843a1d1379d3ebd516fd0317acf2969dafc9
thavamani18 <thavamani.tamtree@gmail.com>
2025-03-07 17:57:16 +0530

    subtraction

--- manipulation.py
+++ manipulation.py
@@ -1,5 +1,11 @@
 #  ADDTION
+# a=5
+# b=4
+# c=a+b
+# print("The addition of two number is",c)
+
+#  SUBTRACTION
 a=5
 b=4
-c=a+b
-print("The addition of two number is",c)
+c=a-b
+print("The subtraction of two number is",c)
